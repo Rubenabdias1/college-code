58c75fb9ffecf2d69e7e3dd25a82be423a507e26
Ruben Nunez <ruben_abdias@live.com>
2021-03-19 12:43:53 -0500

    Delete unrelated directories

--- CS 120 - Introduction to Computer Science I/Chapter 2/Ch_02_Student_Files/example2.py
+++
@@ -1,9 +0,0 @@
-STATE_RATE = 0.03
-COUNTY_RATE = 0.02
-
-propertyValue = float(input("Enter the property value: "))
-stateTax = propertyValue * STATE_RATE
-countyTax = propertyValue * COUNTY_RATE
-totalTax = stateTax + countyTax
-
-print("Property Tax: $" + str(totalTax))
