3795c20444b3b92d81fc9452bb12f18173fde8d5
Ruben Nunez <ruben_abdias@live.com>
2021-03-19 12:40:40 -0500

    Delete unrelated directories

--- CS 120 - Introduction to Computer Science I/Chapter 2/Ch_02_Student_Files/ex7.py
+++
@@ -1,8 +0,0 @@
-DAYSPYEAR = 365
-HOURSPDAY = 24
-MINUTESPHOUR = 60
-
-hours = DAYSPYEAR * HOURSPDAY
-minutes = hours * MINUTESPHOUR
-
-print("The number of minutes in a year are: " + str(minutes), "minutes")
--- CS 120 - Introduction to Computer Science I/Chapter 3/Ch_03_Student_Files/sum.py
+++
@@ -1,7 +0,0 @@
-theSum = 0.0
-data = input("Enter a number: ")
-while data != "":
-    number = float(data)
-    theSum += number
-    data = input("Enter the next number: ")
-print("The sum is", theSum)
