b5db71056baf41e792125ab0207e743b82a2ceab
Ruben Nunez <ruben_abdias@live.com>
2021-04-21 12:00:27 -0500

    move book apps projects

---
+++ CS 314 - Object Oriented Programming in Java/Chapter 12/src/exercises/Employee.java
@@ -0,0 +1,36 @@
+package exercises;
+
+public class Employee implements Displayable, DepartmentsConstants {
+
+    private int department;
+    private String firstName;
+    private String lastName;
+
+    public Employee(int department, String lastName, String firstName) {
+        this.department = department;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+    
+    @Override
+    public String toString() {
+    	String text = "";
+    	String dept = "";
+    	
+    	text += firstName +  " " + lastName;
+    	
+    	if (department == ADMIN) {
+    		dept = "Administration";
+    	}
+    	else if (department == EDITORIAL) {
+    		dept = "Editorial";
+    	}
+    	else if (department == MARKETING) {
+    		dept = "Marketing";
+    	}
+    	
+    	text += " (" + dept + ")";
+    	
+    	return text;
+    }
+}
