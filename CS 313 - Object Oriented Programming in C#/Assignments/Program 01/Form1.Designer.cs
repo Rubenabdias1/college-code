4ebe81046f0744132f82b5af68692aab7d793155
Ruben Nunez <ruben_abdias@live.com>
2021-09-22 00:34:37 -0500

    CS 313 Visual C# Program 1 5 images assignments

---
+++ CS 313 - Object Oriented Programming in C#/Assignments/Program 01/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Program_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numberOnePictureBox = new System.Windows.Forms.PictureBox();
+            this.numberTwoPictureBox = new System.Windows.Forms.PictureBox();
+            this.numberThreePictureBox = new System.Windows.Forms.PictureBox();
+            this.numberFourPictureBox = new System.Windows.Forms.PictureBox();
+            this.numberFivePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOnePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberTwoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberThreePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberFourPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberFivePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numberOnePictureBox
+            // 
+            this.numberOnePictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numberOnePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("numberOnePictureBox.Image")));
+            this.numberOnePictureBox.Location = new System.Drawing.Point(12, 12);
+            this.numberOnePictureBox.Name = "numberOnePictureBox";
+            this.numberOnePictureBox.Size = new System.Drawing.Size(80, 144);
+            this.numberOnePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.numberOnePictureBox.TabIndex = 0;
+            this.numberOnePictureBox.TabStop = false;
+            this.numberOnePictureBox.Click += new System.EventHandler(this.numberOnePictureBox_Click);
+            // 
+            // numberTwoPictureBox
+            // 
+            this.numberTwoPictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numberTwoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("numberTwoPictureBox.Image")));
+            this.numberTwoPictureBox.Location = new System.Drawing.Point(111, 13);
+            this.numberTwoPictureBox.Name = "numberTwoPictureBox";
+            this.numberTwoPictureBox.Size = new System.Drawing.Size(80, 144);
+            this.numberTwoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.numberTwoPictureBox.TabIndex = 1;
+            this.numberTwoPictureBox.TabStop = false;
+            this.numberTwoPictureBox.Click += new System.EventHandler(this.numberTwoPictureBox_Click);
+            // 
+            // numberThreePictureBox
+            // 
+            this.numberThreePictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numberThreePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("numberThreePictureBox.Image")));
+            this.numberThreePictureBox.Location = new System.Drawing.Point(213, 13);
+            this.numberThreePictureBox.Name = "numberThreePictureBox";
+            this.numberThreePictureBox.Size = new System.Drawing.Size(80, 144);
+            this.numberThreePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.numberThreePictureBox.TabIndex = 2;
+            this.numberThreePictureBox.TabStop = false;
+            this.numberThreePictureBox.Click += new System.EventHandler(this.numberThreePictureBox_Click);
+            // 
+            // numberFourPictureBox
+            // 
+            this.numberFourPictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numberFourPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("numberFourPictureBox.Image")));
+            this.numberFourPictureBox.Location = new System.Drawing.Point(316, 13);
+            this.numberFourPictureBox.Name = "numberFourPictureBox";
+            this.numberFourPictureBox.Size = new System.Drawing.Size(80, 144);
+            this.numberFourPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.numberFourPictureBox.TabIndex = 3;
+            this.numberFourPictureBox.TabStop = false;
+            this.numberFourPictureBox.Click += new System.EventHandler(this.numberFourPictureBox_Click);
+            // 
+            // numberFivePictureBox
+            // 
+            this.numberFivePictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numberFivePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("numberFivePictureBox.Image")));
+            this.numberFivePictureBox.Location = new System.Drawing.Point(418, 13);
+            this.numberFivePictureBox.Name = "numberFivePictureBox";
+            this.numberFivePictureBox.Size = new System.Drawing.Size(80, 144);
+            this.numberFivePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.numberFivePictureBox.TabIndex = 4;
+            this.numberFivePictureBox.TabStop = false;
+            this.numberFivePictureBox.Click += new System.EventHandler(this.numberFivePictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 169);
+            this.Controls.Add(this.numberFivePictureBox);
+            this.Controls.Add(this.numberFourPictureBox);
+            this.Controls.Add(this.numberThreePictureBox);
+            this.Controls.Add(this.numberTwoPictureBox);
+            this.Controls.Add(this.numberOnePictureBox);
+            this.Name = "Form1";
+            this.Text = "Numbers";
+            ((System.ComponentModel.ISupportInitialize)(this.numberOnePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberTwoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberThreePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberFourPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberFivePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox numberOnePictureBox;
+        private System.Windows.Forms.PictureBox numberTwoPictureBox;
+        private System.Windows.Forms.PictureBox numberThreePictureBox;
+        private System.Windows.Forms.PictureBox numberFourPictureBox;
+        private System.Windows.Forms.PictureBox numberFivePictureBox;
+    }
+}
+
---
+++ CS 313 - Object Oriented Programming in C#/Assignments/Program 01/Form1.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Ruben Nunez
+ * CS 313 01
+ * Program Assignment 01
+ * Wednesday, September 22, 2021
+ * This program contains images of the numbers from 1 to 5. If an user clicks
+ * any of the numbers, it is going to show a message box containing the english
+ * spelling of the number that was clicked.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numberOnePictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("One");
+        }
+
+        private void numberTwoPictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Two");
+        }
+
+        private void numberThreePictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Three");
+        }
+
+        private void numberFourPictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Four");
+        }
+
+        private void numberFivePictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Five");
+        }
+    }
+}
