3a9cde8e6f433f74fa64fcf64c12148b272990e0
Ruben Nunez <ruben_abdias@live.com>
2022-02-02 16:20:49 -0600

    cs225 upload assignment 1

---
+++ CS 225 - Secure Programming/fig02_04.c
@@ -0,0 +1,22 @@
+// Addition program.
+#include <stdio.h>
+
+// function main begins program execution 
+int main(void) {
+    int integer1 = 0; // will hold first number user enters 
+    int integer2 = 0; // will hold second number user enters
+ 
+    printf("Enter first integer: "); // prompt
+    scanf("%d", &integer1); // read an integer
+    printf("Enter second integer: "); // prompt
+    scanf("%d", &integer2); // read an integer
+ 
+    int sum = 0; // variable in which sum will be stored
+    sum = integer1 + integer2; // assign total to sum
+ 
+    printf("Sum is %d\n", sum); // print sum
+    
+    double average =  (double)sum / (double)2; // compute average
+    
+    printf("Average is %f\n", average); // print average
+} // end function main6
